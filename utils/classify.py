7600c2de05d48083d7a3765650dd3df951eebc7f
DatTran0509 <dat881070@gmail.com>
2024-12-23 22:04:54 +0700

    Update Preprocessing And LSTM

--- utils/classify.py
+++ utils/classify.py
@@ -5,13 +5,3 @@ def feedbackSentimentAnalysis(result):
         return "Trung Lập"
     elif result == 2:
         return "Tích Cực"
-
-def feedbackTopic(result):
-    if result == 0:
-        return "Giảng Viên"
-    elif result == 1:
-        return "Chương trình học"
-    elif result == 2:
-        return "Trang thiết bị"
-    elif result == 3:
-        return "Khác"
